670a5cd67fa3e700f34465c640b987f3b9f05650
ImGabreuw <imgabreuw.dev@gmail.com>
2023-05-15 17:54:23 -0300

    refactor: clean code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src.app.cli.confirm_vote_cli import ConfirmVoteCLI
 from src.app.cli.facade.candidate_facade import CandidateFacade
+from src.app.cli.facade.check_results_facade import CheckResultsFacade
 from src.app.cli.facade.menu_facade import MenuFacade
 from src.app.cli.facade.report_facade import ReportFacade
 from src.app.cli.facade.voter_facade import VoterFacade
 from src.app.cli.facade.voting_facade import VotingFacade
 from src.app.config.template_loader import TemplateLoader
 from src.domain.service.candidate_service import CandidateService
+from src.domain.service.check_results_service import CheckResultService
 from src.domain.service.voter_service import VoterService
 from src.domain.service.voting_service import VotingService
 
@@ -15,36 +17,41 @@ if __name__ == '__main__':
     voter_service = VoterService()
     confirm_vote_cli = ConfirmVoteCLI(template_loader, candidate_service)
     voting_service = VotingService(candidate_service, voter_service)
+    check_results_service = CheckResultService(candidate_service, voter_service)
+
+    menu_facade = MenuFacade(template_loader)
+    candidate_facade = CandidateFacade(candidate_service)
+    voting_facade = VotingFacade(
+        confirm_vote_cli,
+        voting_service,
+        voter_service,
+        candidate_service
+    )
+    check_results_facade = CheckResultsFacade(template_loader, check_results_service)
+    report_facade = ReportFacade(
+        candidate_service,
+        voter_service,
+        template_loader
+    )
 
     while True:
-        selected_option = MenuFacade(template_loader).show_menu()
+        selected_option = menu_facade.show_menu()
 
         if selected_option == 1:
-            CandidateFacade(candidate_service).register_candidates()
-
-            for candidate in candidate_service.find_all():
-                print(candidate)
+            candidate_facade.register_candidates()
 
         if selected_option == 2:
             VoterFacade(voter_service).register_voters()
 
-            for voter in voter_service.find_all():
-                print(voter)
-
         if selected_option == 3:
-            VotingFacade(
-                confirm_vote_cli,
-                voting_service,
-                voter_service,
-                candidate_service
-            ).voting()
+            voting_facade.voting()
+
+        if selected_option == 4:
+            check_results_facade.show_winners()
+            check_results_facade.show_ranking()
 
         if selected_option == 5:
-            ReportFacade(
-                candidate_service,
-                voter_service,
-                template_loader
-            ).show_report()
+            report_facade.show_report()
 
         if selected_option == 6:
             print(template_loader.get_template("end"))
