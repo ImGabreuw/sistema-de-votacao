9c245c87f38c59eb026ce357962a90fdca4db343
ImGabreuw <imgabreuw.dev@gmail.com>
2023-05-23 16:27:07 -0300

    tests: add e2e test for voting, check results and report

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,15 +1,23 @@
+from typing import List, Dict
+
 import pyautogui
 
 from src.domain.entities.role import Role
 from src.shared.helper.faker_helper import get_fake_instance, generate_unique_cpf
 from src.shared.helper.pyautogui_helper import write_multiple_lines
 
+used_candidates_number: Dict[Role, List[int]] = {
+    Role.PRESIDENT: [-2, -1],
+    Role.GOVERNOR: [-2, -1],
+    Role.MAYOR: [-2, -1],
+}
+
 fake = get_fake_instance()
 
 
 def open_and_run_main_py():
     pyautogui.press(["shift", "shift"])
-    pyautogui.PAUSE = 1.5
+    pyautogui.PAUSE = 2.5
 
     pyautogui.write("main.py")
     pyautogui.PAUSE = 1
@@ -27,11 +35,17 @@ def register_candidates(n: int):
     pyautogui.press("enter")
 
     for i in range(1, n + 1):
+        candidate_number = fake.unique.random_int(min=1)
+        role = list(Role)[fake.random_int(min=0, max=2)]
+
+        if candidate_number not in used_candidates_number[role]:
+            used_candidates_number[role].append(candidate_number)
+
         write_multiple_lines(
             fake.unique.name(),
-            fake.unique.random_int(min=1),
+            candidate_number,
             fake.company(),
-            list(Role)[fake.random_int(min=0, max=2)].value,
+            role.value,
         )
 
         pyautogui.write("NAO" if i == n else "SIM")
@@ -41,6 +55,9 @@ def register_candidates(n: int):
 
 
 def register_voters(n: int):
+    global number_voters
+    number_voters = n
+
     pyautogui.write("2")
     pyautogui.press("enter")
 
@@ -56,7 +73,46 @@ def register_voters(n: int):
     pyautogui.PAUSE = 0.5
 
 
+def voting():
+    pyautogui.write("3")
+    pyautogui.press("enter")
+
+    for _ in range(number_voters):
+        for role in Role:
+            rand_index = fake.random_int(min=0, max=len(used_candidates_number[role]) - 1)
+            candidate_number = used_candidates_number[role][rand_index]
+
+            pyautogui.write(str(candidate_number))
+            pyautogui.press("enter")
+            pyautogui.PAUSE = 0.5
+
+            pyautogui.write("SIM")
+            pyautogui.press("enter")
+            pyautogui.PAUSE = 0.5
+
+
+def voting_results():
+    pyautogui.write("4")
+    pyautogui.press("enter")
+    pyautogui.PAUSE = 0.5
+
+
+def report():
+    pyautogui.write("5")
+    pyautogui.press("enter")
+    pyautogui.PAUSE = 0.5
+
+
+def end():
+    pyautogui.write("6")
+    pyautogui.press("enter")
+
+
 if __name__ == '__main__':
     open_and_run_main_py()
     register_candidates(3)
     register_voters(3)
+    voting()
+    voting_results()
+    report()
+    end()
